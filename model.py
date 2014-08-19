cfcb87c925db630678a4b8b08eceecd7c0d2a930
Daniel Petersen <daniel.petersen@rapidmediaproductions.com>
2014-08-19 14:37:24 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,9 @@
 from sklearn.cross_validation import cross_val_score
 from sklearn.tree import DecisionTreeRegressor as Model
 from sklearn.externals import joblib
-import config
 import numpy
-import parse
 import sys
+import config
 
 def train(features, target):
     model = Model()
@@ -57,31 +56,33 @@ def RootMeanSquareError(predicted_values, true_values):
     return numpy.sqrt(residuals/n)
     
 if __name__ == '__main__':
+    '''
+        call like 
+        python model.py IBM 5DayWindowBestLongBuyPrice > test.txt
+        python model.py GSPC 5DayWindowBestLongBuyPrice IBM > test.txt
+    '''
+    
     ticker = sys.argv[1]
     target = sys.argv[2]
     target_ticker = sys.argv[3] if len(sys.argv) > 3 and sys.argv[3] != '>' else ticker
     
-    train_features_file = "%s_training_features.npy" % ticker
-    train_targets_file = "%s_training_target_%s.npy" % (target_ticker, target)
-    test_features_files = "%s_testing_features.npy" % ticker
-    test_targets_file = "%s_testing_target_%s.npy" % (target_ticker, target)
+    train_features_file = config.GetTrainingFeaturesFileName(ticker)
+    train_targets_file = config.GetTrainingTargetsFileName(target_ticker, target)
+    test_features_file = config.GetTestingFeaturesFileName(ticker)
+    test_targets_file = config.GetTestingTargetsFileName(target_ticker, target)
+    model_file = config.GetModelFileName(ticker, target, target_ticker)
+    predictions_file = config.GetPredictionsFileName(ticker, target, target_ticker)
        
     print "Loading data..."
     train_features = numpy.load(train_features_file)
     train_targets = numpy.load(train_targets_file)
-    test_features = numpy.load(test_features_files)
+    test_features = numpy.load(test_features_file)
     test_targets = numpy.load(test_targets_file)
     
     print "Training model..."
     model = train(train_features, train_targets)
     print "Saving model to file..."
-    joblib.dump(model, "%s_%s_model.pkl" % (ticker, target))
-    # reconstitute via model = joblib.load("%s_%s_model.pkl" % (ticker, target))
-    
-    feature_params = model.feature_importances_
-    print "Feature, Importance"
-    for i in range(len(config.features)):
-        print config.features[i], feature_params[i]
+    joblib.dump(model, model_file)
     
     print "Making predictions..."
     predictions = predict(model, test_features)
