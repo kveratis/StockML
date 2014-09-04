a9ae6c7fc0bd93369a22c275d60bb4f07272b63e
Daniel Petersen <daniel.petersen@rapidmediaproductions.com>
2014-09-04 10:34:18 -0600

    Fix bug in splitting by year

--- preprocess.py
+++ preprocess.py
@@ -3,6 +3,7 @@ import parse
 import sys
 import numpy
 import operator
+from datetime import datetime, date, time
   
 def extract_new_feature_name(feature, targetValue=None, newFeatureName=None):
     if newFeatureName != None:
@@ -50,18 +51,21 @@ def extract_target_matrix(data, target, defaultValue=0):
         i+=1
     return matrix
     
-def extract_all_targets_into_file(data, targets, fileNamePattern, prefix,  defaultValue=0):
+def extract_all_targets_into_file(data, targets, ticker, fileNameGenFunction, defaultValue=0):
     for target in targets:
         m = extract_target_matrix(data, target, defaultValue)
-        numpy.save(fileNamePattern % (prefix, target), m)
+        file_name = fileNameGenFunction(ticker, target)
+        numpy.save(file_name, m)
     
 def partitionDataByFeatureRange(data, feature, trainingStartRange, testingStartRange):
     training_set = []
     testing_set = []
     
     for row in data:
-        if row[feature] >= trainingStartRange:
-            if row[feature] < testingStartRange:
+        year = datetime.strptime(row["Date"], "%Y-%m-%d").year
+        #if row[feature] >= trainingStartRange:
+        if year >= trainingStartRange:
+            if year < testingStartRange:
                 training_set.append(row)
             else:
                 testing_set.append(row)
@@ -73,24 +77,22 @@ if __name__ == '__main__':
     Call like python preprocess.py BAC 1993 2014
     """
     ticker = sys.argv[1]
-    trainLowerBoundDate = sys.argv[2]
-    testLowerBoundDate = sys.argv[3]
+    trainLowerBoundDate = int(sys.argv[2])
+    testLowerBoundDate = int(sys.argv[3])
     
     data_file = config.GetDataFileName(ticker)
     train_features_file = config.GetTrainingFeaturesFileName(ticker)
-    train_targets_file = config.GetTrainingTargetsFileName(target_ticker, target)
     test_features_files = config.GetTestingFeaturesFileName(ticker)
-    test_targets_file = config.GetTestingTargetsFileName(target_ticker, target)
     
     data = parse.readCsvFile(data_file)
     training_set, testing_set = partitionDataByFeatureRange(data, "Year", trainLowerBoundDate, testLowerBoundDate)
-    
+        
     training_feats = extract_feature_matrix(training_set, config.features)
     numpy.save(train_features_file, training_feats)
     
-    extract_all_targets_into_file(training_set, config.regression_targets, train_targets_file)
+    extract_all_targets_into_file(training_set, config.regression_targets, ticker, config.GetTrainingTargetsFileName)
     
     testing_feats = extract_feature_matrix(testing_set, config.features)
     numpy.save(test_features_files, testing_feats)
     
-    extract_all_targets_into_file(testing_set, config.regression_targets, test_targets_file)
+    extract_all_targets_into_file(testing_set, config.regression_targets, ticker, config.GetTestingTargetsFileName)
