6caaba2ec12182ad71ad21d00c9af3af361edd8c
Daniel Petersen <kveratis@gmail.com>
2014-10-02 14:14:38 -0600

    Add functions for determining how many contracts can be purchased for a given amount of money

--- options.py
+++ options.py
@@ -10,6 +10,9 @@ def roundup(value, precision):
     
 def calcOptionTradeCost(contracts, costPerTrade = 6.95, costPerContract = 0.75):
     return costPerTrade + (contracts * costPerContract)
+    
+def calcCostPerOptionContract(buyPrice, costPerContract = 0.75):
+    return buyPrice * 100 + costPerContract
 
 def calcTotalCost(buyPrice, contracts, costPerTrade = 6.95, costPerContract = 0.75):
     return buyPrice * contracts * 100 + calcOptionTradeCost(contracts, costPerTrade, costPerContract)
@@ -37,13 +40,19 @@ def calcMinimumOptionInvestmentToRealizeProfit(buyPrice, sellPrice, profit, cost
     contractsForProfit = math.ceil(profit / (((sellPrice - buyPrice) * 100) - costPerContract))
     contracts = math.ceil(contractsForBreakEven + contractsForProfit)
     return (calcTotalCost(buyPrice, contracts, costPerTrade, costPerContract), contracts)
+    
+def calcNumberOfOptionsPurchasable(buyPrice, cash, costPerTrade = 6.95, costPerContract = 0.75):
+    itemCost = calcCostPerOptionContract(buyPrice, costPerContract)
+    availableAssets = cash - costPerTrade;
+    return math.floor(availableAssets / itemCost)
         
 if __name__ == '__main__':
     '''
         call like 
-        python options.py [-p/-t] [buyPrice] [sellPrice] [profit]
-            -p profit
-            -t target sell price
+        python options.py [buyPrice] [sellPrice] [contracts]        # Calculates profit from trade info
+        python options.py [-p] [buyPrice] [sellPrice] [profit]      # Calculates number of contracts to achieve target profit level from range 
+        python options.py [-t] [buyPrice] [contracts] [profit]      # Calculates target sell price to achieve break even and target profit level
+        python options.py [-m] [buyPrice] [cashToInvest]            # Calculates the maximum number of contracts you can purchase given the buyPrice and cash        
     '''
     buyPrice = float(sys.argv[2])
     profit = float(sys.argv[4]) if len(sys.argv) > 4 else 0
@@ -64,6 +73,12 @@ if __name__ == '__main__':
         
         minimumInvestment, minimumContracts = calcMinimumOptionInvestmentToRealizeProfit(buyPrice, sellPrice, profit)
         print "Buy: %.2f Sell: %.2f Profit Target Investment: %.2f Contracts: %d Profit: %.2f" % (buyPrice, sellPrice, minimumInvestment, minimumContracts, calcNetProfitOnOption(buyPrice, sellPrice, minimumContracts))
+    elif sys.argv[1] == "-m":
+        cash = float(sys.argv[3])
+        contracts = calcNumberOfOptionsPurchasable(buyPrice, cash)
+        totalCost = calcTotalCost(buyPrice, contracts)
+        
+        print "You can buy %d Contracts at %.2f for %.2f" % (contracts, buyPrice, totalCost)
     else:
         buyPrice = float(sys.argv[1])
         sellPrice = float(sys.argv[2])
